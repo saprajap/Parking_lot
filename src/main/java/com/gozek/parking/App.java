8447efc86bb84d3d549eae68136e60e7d9a99c15
Sanjay Prajapati <raaz.vns@gmail.com>
2019-08-25 15:29:01 +0530

    first commit

---
+++ src/main/java/com/gozek/parking/App.java
@@ -0,0 +1,13 @@
+package com.gozek.parking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
